3287a21b1a134fe20c4e7c2178e6b8d8530b7a76
RichCode <chineduogada@gmail.com>
2021-01-07 15:29:14 -0800

    feature(Carousel): implement component
    [Continue #1]

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,17 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
+import { Fader } from "react-rapid-carousel";
+
+const images = ["/img/carousel-img-1.jpg", "/img/carousel-img-2.jpg"];
 
 const Carousel = ({ ...rest }) => {
   return (
-    <Box
-      border="1px"
-      color="red.400"
-      m={1}
-      rounded="lg"
-      minH={300}
-      opacity=".3"
-      {...rest}
-    >
-      Carousel
+    <Box m={1} rounded="lg" overflow="hidden" {...rest}>
+      <Fader buttons>
+        {images.map((img, index) => (
+          <Box h={300} key={index}>
+            <Image src={img} />
+          </Box>
+        ))}
+      </Fader>
     </Box>
   );
 };
--- context/Providers.js
+++ context/Providers.js
@@ -1,9 +1,21 @@
 import * as React from "react";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { ThemeProvider } from "react-rapid-carousel";
 
 const Providers = ({ children }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <ThemeProvider
+        theme={{
+          dots: { 1: "white", 2: "gray" },
+          carets: { 1: "#333", 2: "transparent" },
+        }}
+      >
+        {children}
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 };
 
 export default Providers;
