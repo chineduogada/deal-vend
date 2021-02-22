842ccb8b2ce496310c6883a1baf64813a63d1c2b
RichCode <chineduogada@gmail.com>
2021-02-22 14:52:15 +0100

    feature(Cart):implement component
    [Finishes #4]

--- context/Providers.js
+++ context/Providers.js
@@ -1,12 +1,39 @@
 import * as React from "react";
-
+import { useReducer } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider } from "react-rapid-carousel";
 import CartContext from "./CartContext";
-import useCart from "../useCart";
 
+const initialState = [];
+
+// let product;
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return [...state, { ...action.payload, itemCount: 1 }];
+
+    // case REMOVE_PRODUCT:
+    //   return state.filter((product) => product.id !== action.payload);
+
+    // case INCREASE_ITEM_COUNT:
+    //   product = state.find((product) => product.id === action.payload);
+    //   product.itemCount += 1;
+
+    //   return state;
+
+    // case DECREASE_ITEM_COUNT:
+    //   product = state.find((product) => product.id === action.payload);
+    //   product.itemCount -= 1;
+
+    //   return state;
+
+    default:
+      return state;
+  }
+};
 const Providers = ({ children }) => {
-  const { state } = useCart();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <ChakraProvider>
@@ -16,7 +43,9 @@ const Providers = ({ children }) => {
           carets: { 1: "white", 2: "#333" },
         }}
       >
-        <CartContext.Provider value={state}>{children}</CartContext.Provider>
+        <CartContext.Provider value={{ state, dispatch }}>
+          {children}
+        </CartContext.Provider>
       </ThemeProvider>
     </ChakraProvider>
   );
--- useCart.js
+++ useCart.js
@@ -1,44 +1,14 @@
-import { useReducer } from "react";
-
-const ADD_PRODUCT = "ADD_PRODUCT";
-const REMOVE_PRODUCT = "REMOVE_PRODUCT";
-const INCREASE_ITEM_COUNT = "INCREASE_ITEM_COUNT";
-const DECREASE_ITEM_COUNT = "DECREASE_ITEM_COUNT";
-
-const initialState = [];
-
-let product;
-
-const reducer = (state, action) => {
-  switch (action.key) {
-    case ADD_PRODUCT:
-      return [...state, { ...action.payload, itemCount: 1 }];
-
-    case REMOVE_PRODUCT:
-      return state.filter((product) => product.id !== action.payload);
-
-    case INCREASE_ITEM_COUNT:
-      product = state.find((product) => product.id === action.payload);
-      product.itemCount += 1;
-
-      return state;
-
-    case DECREASE_ITEM_COUNT:
-      product = state.find((product) => product.id === action.payload);
-      product.itemCount -= 1;
-
-      return state;
-
-    default:
-      return state;
-  }
-};
+import { useContext } from "react";
+import CartContext from "./context/CartContext";
 
 const useCart = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { state, dispatch } = useContext(CartContext);
 
-  const handleAddProduct = (payload) =>
-    dispatch({ type: ADD_PRODUCT, payload });
+  const handleAddProduct = (payload) => {
+    // console.log(payload);
+    dispatch({ type: "ADD_PRODUCT", payload });
+  };
+
   const handleRemoveProduct = (payload) =>
     dispatch({ type: REMOVE_PRODUCT, payload });
   const handleIncreaseItemCount = (payload) =>
