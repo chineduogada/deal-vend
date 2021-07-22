bf38927bae245528e0977d7d983ea1fa7e574726
RichCode <chineduogada@gmail.com>
2021-07-22 14:26:04 +0100

    bug: fix hydration error
    [Start #13]

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,26 @@
 import { Box } from "@chakra-ui/react";
 import Header from "./Header";
 import Footer from "./Footer";
+import usePageReady from "../hooks/usePageReady";
 
 const Layout = ({ children, showHeaderCenter, breadcrumbPaths }) => {
+  const pageReady = usePageReady();
+
   return (
-    <Box bg="gray.100" pt={2}>
-      <Box px={2} m="0 auto" maxW={900} bg="gray.100">
-        <Header
-          showHeaderCenter={showHeaderCenter}
-          breadcrumbPaths={breadcrumbPaths}
-        />
+    pageReady && (
+      <Box bg="gray.100" pt={2}>
+        <Box px={2} m="0 auto" maxW={900} bg="gray.100">
+          <Header
+            showHeaderCenter={showHeaderCenter}
+            breadcrumbPaths={breadcrumbPaths}
+          />
 
-        <Box as="main">{children}</Box>
+          <Box as="main">{children}</Box>
 
-        <Footer />
+          <Footer />
+        </Box>
       </Box>
-    </Box>
+    )
   );
 };
 
